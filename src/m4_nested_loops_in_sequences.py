eb3af7df9059bbe492af4feb80631bf3c4d51e6e
CodeCactus <groomlc@rose-hulman.edu>
2019-03-28 14:20:35 -0400

    DONEZO

--- src/m4_nested_loops_in_sequences.py
+++ src/m4_nested_loops_in_sequences.py
@@ -3,8 +3,8 @@ This project demonstrates NESTED LOOPS (i.e., loops within loops)
 in the context of SEQUENCES OF SUB-SEQUENCES.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and liam groom.
+"""  # : 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -128,7 +128,9 @@ def multiply_numbers(sequence_of_lists):
     #             to loop through it in the INNER loop.
     #        -- See   m2e_nested_loops_in_sequences   as needed.
     # ------------------------------------------------------------------
-
+    for k in range(len(sequence_of_lists)):
+        for j in range(len(sequence_of_lists[k])):
+            sequence_of_lists[k][j]=sequence_of_lists[k][j]*(k+1)
 
 def run_test_sum_numbers():
     """ Tests the    sum_numbers    function. """
@@ -154,7 +156,6 @@ def run_test_sum_numbers():
 
     # TO DO 3 (continued): Add your ADDITIONAL test(s) here:
 
-
 def sum_numbers(seq_seq):
     """
     Returns the sum of the numbers in the given sequence
@@ -169,7 +170,14 @@ def sum_numbers(seq_seq):
     # TODO: 4. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
+    sume=0
+    for k in range(len(seq_seq)):
+        val=0
+        for j in range(len(seq_seq[k])):
+            val=val+seq_seq[k][j]
+        sume = sume + val
 
+    return sume
 
 def run_test_print_characters():
     """ Tests the    print_characters    function. """
@@ -217,11 +225,13 @@ def print_characters(sequence_of_strings):
     Precondition:  the given argument is a sequence of strings.
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # TDO: 5. Implement and test this function.
     #  ** READ THE TESTS that have been written for you (ABOVE).
     #  ** ASK QUESTIONS if you do not understand the TESTS (ABOVE).
     # ------------------------------------------------------------------
-
+    for k in range(len(sequence_of_strings)):
+        for j in range(len(sequence_of_strings[k])):
+            print(sequence_of_strings[k][j])
 
 def run_test_print_characters_slanted():
     """ Tests the    print_characters_slanted    function. """
@@ -270,14 +280,16 @@ def print_characters_slanted(sequence_of_strings):
     Precondition:  the given argument is a sequence of strings.
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # TDO: 6. Implement and test this function.
     #  ** READ THE TESTS that have been written for you (ABOVE).
     #  ** ASK QUESTIONS if you do not understand the TESTS (ABOVE).
     #
     # ** HINT: ** Consider using string multiplication for the spaces
     #             and string addition to stitch the spaces to the character.
     # ------------------------------------------------------------------
-
+    for k in range(len(sequence_of_strings)):
+        for j in range(len(sequence_of_strings[k])):
+            print(' '*j+sequence_of_strings[k][j])
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
